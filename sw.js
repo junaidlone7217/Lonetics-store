2e5db77ae49e496591c19023eb690229e208d969
junaidlone7217 <junaidlone7217@gmail.com>
2026-07-16 14:11:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Installed');
+});
+
+self.addEventListener('activate', (e) => {
+    console.log('[Service Worker] Activated');
+});
+
+// Chrome REQUIRES this fetch event to trigger the Install Prompt
+self.addEventListener('fetch', (e) => {
+    // This allows the app to pass the PWA installation requirements
+});
 // Service Worker
